80b302e03923623dae37b2bec4c2fc4501268395
PlayerG9 <g9yout@gmail.com>
2023-11-25 10:13:26 +0100

    fixed arguments

--- src/shell_complete/argparse/action.py
+++ src/shell_complete/argparse/action.py
@@ -7,10 +7,12 @@ from ..core import generate
 
 
 class ActionShellComplete(ap.Action):
+    # noinspection PyShadowingBuiltins
     def __init__(self, option_strings, dest, help=None):
         super().__init__(option_strings, dest=ap.SUPPRESS, nargs=0, help=help)
 
-    def __call__(self, parser: ap.ArgumentParser, namespace: ap.Namespace, values, option_string=None):
-        print([namespace, values, option_string])
+    # (parser, namespace, values, option_string)
+    # but we only need `parser`
+    def __call__(self, parser: ap.ArgumentParser, *_, **__):
         print(generate(parser))
         parser.exit()
