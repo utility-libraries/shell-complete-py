338d5eff4eaa83ba41c9e38d5f3a37634e4fda49
PlayerG9 <g9yout@gmail.com>
2023-11-26 14:13:49 +0100

    v0 of script generator

--- src/shell_complete/core/_writer.py
+++ src/shell_complete/core/_writer.py
@@ -3,6 +3,7 @@ r"""
 
 """
 import io
+import textwrap
 import typing as t
 import contextlib
 from shlex import quote
@@ -16,18 +17,22 @@ class BashWriter:
     def __init__(self, program: str):
         self._program = program
         self._indentation = 0
-        self.stream = io.StringIO()
+        self._stream = io.StringIO()
 
     @cached_property
-    def safe_program(self):
+    def _safe_program(self) -> str:
         import re
         return re.sub(r'\W', '_', self._program)
 
-    def __str__(self):
-        return self.stream.getvalue()
+    @cached_property
+    def _complete_function(self) -> str:
+        return f"__{self._safe_program}_completions"
 
-    def __call__(self, line: str = "", *, xend="\n"):
-        self.stream.write(('  ' * self._indentation) + line + xend)
+    def __str__(self):
+        return self._stream.getvalue()
+
+    def __call__(self, *parts, sep=" ", end="\n"):
+        self._stream.write(('  ' * self._indentation) + sep.join(parts) + end)
 
     @contextlib.contextmanager
     def indent(self) -> t.ContextManager:
@@ -39,23 +44,80 @@ class BashWriter:
 
     def __enter__(self):
         # adds the head
-        self(fr'#!/usr/bin/env bash')
+        self('#!/usr/bin/env bash')
+        self.comment_block(
+            "=Generated with shell-complete",
+            "-----",
+            "https://pypi.org/project/shell-complete/",
+            "https://github.com/utility-libraries/shell-complete-py",
+        )
         self()
-        self(fr'__{self.safe_program} () {{')
+        self('if ! command -v', self._program, '&> /dev/null; then')
+        with self.indent():
+            self('return')
+        self('fi')
+        self()
+        self(self._complete_function, '() {')
         self._indentation += 1
+        self('local CURRENT=${COMP_WORDS[$COMP_CWORD]}')
+        self('local LAST=${COMP_WORDS[$((COMP_CWORD - 1))]}')
+        self('local LINE=${COMP_LINE}')
+        self()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         # adds the tail
         self._indentation -= 1
-        self(fr'}}')
+        self('}')
         self()
-        self(fr'# complete is a bash builtin, but recent versions of ZSH come with a function')
-        self(fr'# called bashcompinit that will create a complete in ZSH. If the user is in')
-        self(fr'# ZSH, load and run bashcompinit before calling the complete function.')
-        self(fr'if [[ -n ${{ZSH_VERSION-}} ]]; then')
+        self.comment("complete is a bash builtin, but recent versions of ZSH come with a function called bashcompinit "
+                     "that will create a complete in ZSH. If the user is in ZSH, load and run bashcompinit before "
+                     "calling the complete function.")
+        self('if [[ -n ${ZSH_VERSION-} ]]; then')
         with self.indent():
-            self(r'autoload -U +X bashcompinit && bashcompinit')
-            self(r'autoload -U +X compinit && compinit')
-        self(fr'fi')
-        self(fr'')
-        self(fr'complete -o default -F __{self.safe_program} {quote(self._program)}')
+            self('autoload -U +X bashcompinit && bashcompinit')
+            self('autoload -U +X compinit && compinit')
+        self('fi')
+        self()
+        self('complete -F', self._complete_function, self._program)
+
+    def comment(self, *lines):
+        width = 80 - self._indentation*2 - 2
+        for line in lines:
+            for wrapped in textwrap.wrap(line, width):
+                self('# ' + wrapped)
+
+    def comment_block(self, *lines):
+        width = 80 - (self._indentation * 2) - 2 - 2
+        self('#' * (width + 4))
+        for line in lines:
+            if line.startswith(3*'='):
+                self('# ' + '=' * width + ' #')
+            elif line.startswith(3*'-'):
+                self('# ' + '-' * width + ' #')
+            elif line.startswith("="):
+                self('# ' + line[1:].center(width) + ' #')
+            else:
+                for wrapped in textwrap.wrap(line, width):
+                    self('# ' + wrapped.ljust(width) + ' #')
+        self('#' * (width + 4))
+
+    @contextlib.contextmanager
+    def switch(self, variable: str):
+        self('case', variable, 'in')
+        try:
+            yield
+        finally:
+            self('esac')
+
+    @contextlib.contextmanager
+    def case(self, *patterns: str):
+        self('|'.join(patterns) + ')')
+        self._indentation += 1
+        try:
+            yield
+        finally:
+            self._indentation -= 1
+            self(';;')
+
+    def complete(self, *options: str, word: str):
+        self('mapfile -t COMPREPLY < <(compgen -W', quote(' '.join(map(quote, options))), '--', word, ')')
--- src/shell_complete/core/generate.py
+++ src/shell_complete/core/generate.py
@@ -3,14 +3,34 @@ r"""
 
 """
 import argparse as ap
-from ._writer import BashWriter
+from ._writer import BashWriter, quote
 
 
 __all__ = ['generate']
 
 
-def generate(parser: ap.ArgumentParser, program: str) -> str:
-    writer = BashWriter(program)
+def generate(parser: ap.ArgumentParser) -> str:
+    writer = BashWriter(program=parser.prog)
     with writer:
-        pass
+
+        # noinspection PyProtectedMember
+        actions = parser._actions
+
+        with writer.switch('"$LAST"'):
+            for action in actions:
+                if action.choices:
+                    with writer.case(*map(quote, action.option_strings)):
+                        writer.complete(*action.choices, word='"$LAST"')
+            with writer.case("*"):
+                writer.complete(
+                    *(
+                        option
+                        for action in actions
+                        for option in action.option_strings
+                        if option[0] not in parser.prefix_chars  # sub-command (prog option)
+                        or option[1] in parser.prefix_chars  # long-option (prog --option)
+                    ),
+                    word='"$CURRENT"',
+                )
+
     return str(writer)
